6b788e0ee2023012a6b3e66e27a97451f9d84158
Janne Järvinen <K1742@student.jamk.fi>
2017-01-16 19:50:33 +0200

    Toka muutos

--- repo1/Program.cs
+++ repo1/Program.cs
@@ -10,7 +10,7 @@ namespace repo1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ASDAAAAA");
+            Console.WriteLine("ASDAAAAA LISAAA");
         }
     }
 }
